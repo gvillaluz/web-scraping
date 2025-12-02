27a1cd015a2663e3c1a4b43b53bd1a5633db1494
kg_dev <kiansalaysay846@gmail.com>
2025-12-02 11:19:15 +0800

    web scraping

---
+++ scraper.py
@@ -0,0 +1,34 @@
+import requests as req
+from datetime import datetime, timedelta
+import pandas as pd
+
+url = 'https://pasig-marikina-tullahanffws.pagasa.dost.gov.ph/water/detail_list.do'
+
+start_time = datetime(2025, 11, 25, 0, 0)
+end_time = datetime(2025, 12, 2, 8, 50)
+
+all_rows = []
+
+obscd = 11104201
+ymdhm = 20251202910
+
+current = end_time
+
+while current >= start_time: 
+    timestamp = current.strftime("%Y%m%d%H%M")
+
+    payload = {
+        "obscd": obscd,
+        "ymdhm": timestamp
+    }
+    
+    resp = req.post(url, data=payload)
+    data = resp.json()
+    
+    if isinstance(data, list) and len(data) > 0:
+        all_rows.extend(data)
+
+    current -= timedelta(minutes=60)
+
+df = pd.DataFrame(all_rows)
+df.to_csv("historical_water_levels.csv", index=False)
